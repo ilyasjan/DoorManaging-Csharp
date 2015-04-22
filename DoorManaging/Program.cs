b66bbdd1295a4e4d9465257d41adb1b9a9d81885
napster <ilyas5137@163.com>
2015-04-22 22:52:15 +0800

    add forms,and basicly designed user interface

--- DoorManaging/Program.cs
+++ DoorManaging/Program.cs
@@ -14,7 +14,7 @@ namespace DoorManaging
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
