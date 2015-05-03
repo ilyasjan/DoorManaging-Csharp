d038011d641f10ba8b6fec9678d3ffecf54d9ba0
ilyasjan <ilyas5137@163.com>
2015-05-03 21:54:12 +0800

    database manage

---
+++ DoorManaging/DBO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoorManaging
+{
+    public sealed class DBO
+    {
+        public static int AddStudent(Entities.Students st)
+        {
+            try
+            {
+                Database db = new Database();
+                db.init();
+                String sql = String.Format("insert into `student`(sno,name,classs,enable) values({0})", st.getSQLString1());
+                return db.Insert(sql);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static int UpdateStudent(Entities.Students stOLd, Entities.Students stNew)
+        {
+            try
+            {
+                Database db = new Database();
+                db.init();
+                String sql = String.Format("update `student` {0} where id={1}", stNew.getSQLString2(), stOLd.ID);
+                return db.Update(sql);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+    }
+}
---
+++ DoorManaging/Entities/Students.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoorManaging.Entities
+{
+    public sealed class Students
+    {
+
+        private long INDEX = -1;
+        public long ID { get { return INDEX; } }
+        public String CARD { get; set; }
+        public Boolean ENABLE { get; set; }
+        public String NAME { get; set; }
+        public String SNO { get; set; }
+        public String CLASS { get; set; }
+        public Students()
+        {
+            INDEX = -1;
+        }
+
+        public Students(long index, String card, String sno, String name, String classs, Boolean enable)
+        {
+            CARD = card;
+            INDEX = index;
+            SNO = sno;
+            NAME = name;
+            CLASS = classs;
+            ENABLE = enable;
+        }
+
+        public String getSQLString1()
+        {
+            return String.Format(("'{0}','{1}','{2}','{3}','{4}'"),this.CARD, this.SNO, this.NAME, this.CLASS, this.ENABLE);
+        }
+        public String getSQLString2()
+        {
+            return String.Format(("card='{0}',sno='{1}',name='{2}',classs='{3}',enable='{4}'"), this.CARD,this.SNO, this.NAME, this.CLASS, this.ENABLE);
+        }
+    }
+}
