8c9164895ea4ac8f5aac30ccab98edeb38c707eb
ilyasjan <ilyas5137@163.com>
2015-05-03 22:25:39 +0800

    hhhh

--- DoorManaging/Forms/AddNew.Designer.cs
+++ DoorManaging/Forms/AddNew.Designer.cs
@@ -30,15 +30,15 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSNO = new System.Windows.Forms.TextBox();
+            this.txtCLASS = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txtCard = new System.Windows.Forms.TextBox();
+            this.chkEnable = new System.Windows.Forms.CheckBox();
             this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -62,26 +62,26 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox1
+            // txtName
             // 
-            this.textBox1.Location = new System.Drawing.Point(74, 56);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(192, 21);
-            this.textBox1.TabIndex = 2;
+            this.txtName.Location = new System.Drawing.Point(74, 56);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(192, 21);
+            this.txtName.TabIndex = 2;
             // 
-            // textBox2
+            // txtSNO
             // 
-            this.textBox2.Location = new System.Drawing.Point(73, 90);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(192, 21);
-            this.textBox2.TabIndex = 3;
+            this.txtSNO.Location = new System.Drawing.Point(73, 90);
+            this.txtSNO.Name = "txtSNO";
+            this.txtSNO.Size = new System.Drawing.Size(192, 21);
+            this.txtSNO.TabIndex = 3;
             // 
-            // textBox3
+            // txtCLASS
             // 
-            this.textBox3.Location = new System.Drawing.Point(73, 125);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(192, 21);
-            this.textBox3.TabIndex = 4;
+            this.txtCLASS.Location = new System.Drawing.Point(73, 125);
+            this.txtCLASS.Name = "txtCLASS";
+            this.txtCLASS.Size = new System.Drawing.Size(192, 21);
+            this.txtCLASS.TabIndex = 4;
             // 
             // label1
             // 
@@ -119,24 +119,24 @@
             this.label4.TabIndex = 9;
             this.label4.Text = "卡号";
             // 
-            // textBox4
+            // txtCard
             // 
-            this.textBox4.Location = new System.Drawing.Point(73, 29);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(192, 21);
-            this.textBox4.TabIndex = 8;
+            this.txtCard.Location = new System.Drawing.Point(73, 29);
+            this.txtCard.Name = "txtCard";
+            this.txtCard.Size = new System.Drawing.Size(192, 21);
+            this.txtCard.TabIndex = 8;
             // 
-            // checkBox1
+            // chkEnable
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Checked = true;
-            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox1.Location = new System.Drawing.Point(73, 152);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(72, 16);
-            this.checkBox1.TabIndex = 21;
-            this.checkBox1.Text = "是否有效";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.chkEnable.AutoSize = true;
+            this.chkEnable.Checked = true;
+            this.chkEnable.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkEnable.Location = new System.Drawing.Point(73, 152);
+            this.chkEnable.Name = "chkEnable";
+            this.chkEnable.Size = new System.Drawing.Size(72, 16);
+            this.chkEnable.TabIndex = 21;
+            this.chkEnable.Text = "是否有效";
+            this.chkEnable.UseVisualStyleBackColor = true;
             // 
             // label5
             // 
@@ -152,16 +152,16 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(289, 229);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.chkEnable);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtCard);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtCLASS);
+            this.Controls.Add(this.txtSNO);
+            this.Controls.Add(this.txtName);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -178,15 +178,15 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSNO;
+        private System.Windows.Forms.TextBox txtCLASS;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox txtCard;
+        private System.Windows.Forms.CheckBox chkEnable;
         private System.Windows.Forms.Label label5;
     }
 }
--- DoorManaging/Forms/AddNew.cs
+++ DoorManaging/Forms/AddNew.cs
@@ -22,7 +22,29 @@ namespace DoorManaging
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            foreach (Control ct in this.Controls)
+            {
+                if (ct is TextBox)
+                    if (((TextBox)ct).Text.Trim().Length < 1)
+                    {
+                        MessageBox.Show("不能为空!", "警告!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+            }
+            try
+            {
+                var st = new Entities.Students();
+                st.CARD = txtCard.Text;
+                st.CLASS = txtCLASS.Text;
+                st.SNO = txtSNO.Text;
+                st.ENABLE = chkEnable.Checked;
+                int i = DBO.AddStudent(st);
+                MessageBox.Show(String.Format("{0}个信息已被添加!", i), "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.Message, "警告!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
