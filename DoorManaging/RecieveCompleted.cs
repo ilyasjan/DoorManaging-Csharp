8b3f0014e3153a2ac4ee0db3913e6894aaf7040a
ilyasjan <ilyas5137@163.com>
2015-05-13 20:57:58 +0600

    oor

---
+++ DoorManaging/RecieveCompleted.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoorManaging
+{
+  public  interface RecieveCompleted
+    {
+      void RecieveCompleted(Byte[] bytes);
+    }
+}
